209f69dd5b562a0477bf2ff5a219c53611d13d5a
Umar <uk.umarkhan1999@gamil.com>
2022-11-30 05:51:52 +0530

    deploy to heroku csv

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var mongoose = require("mongoose")
+let express = require("express")
+let bodyParser = require("body-parser")
+let mongoose = require("mongoose")
+let { Parser } = require('json2csv')
+let fs = require('fs')
+const { parse } = require("path")
+
 const port  = process.env.PORT || 3000;
 
 const app = express()
@@ -22,11 +26,11 @@ var db = mongoose.connection;
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
 
-let enrglb;
-let examglb;
-let courseglb;
+let enrglobal;
+let examglobal;
+let courseglobal;
 
-app.post("/create",(req,res)=>{
+app.post("/createstudent",(req,res)=>{  //add new student
     let name = req.body.name;
     let enr = req.body.enr;
     let course = req.body.course;
@@ -35,10 +39,8 @@ app.post("/create",(req,res)=>{
         "_id" : enr,
         "name": name,
         "midsem":{
-
         },
         "endsem":{
-
         }
     }
 
@@ -46,31 +48,31 @@ app.post("/create",(req,res)=>{
         if(err){
             throw err;
         }
-        console.log("Record Inserted Successfully");
+        console.log(`${enr} inserted successfully in ${course}`);
     });
 
-    return res.redirect('/');
+    return res.redirect('/addstudent.html');
 
 })
-app.post("/select",(req,res)=>{
-    enrglb = req.body.enr;
-    courseglb = req.body.course;
-    examglb = req.body.exam;
-    return res.redirect('/insert.html')
+app.post("/updatestudent",(req,res)=>{  //update student
+    enrglobal = req.body.enr;
+    courseglobal = req.body.course;
+    examglobal = req.body.exam;
+    return res.redirect('/updatestudent.html')
 
 })
 
-app.post("/insert",async(req,res)=>{
+app.post("/addquestion",async(req,res)=>{
     let co = req.body.co;
     let qid = req.body.qid;
     let marksOb = req.body.marksOb;
     let marksTotal = req.body.marksTotal;
-    console.log(courseglb);
-    const filter = {_id:enrglb};
+    console.log(courseglobal);
+    const filter = {_id:enrglobal};
     const options = { upsert: true };
-    const course = db.collection(courseglb.toString());
-    const marksO = `${examglb}.${co}.${qid}.marksObtained`;
-    const marksT = `${examglb}.${co}.${qid}.totalMarks`;
+    const course = db.collection(courseglobal.toString());
+    const marksO = `${examglobal}.${co}.${qid}.marksObtained`;
+    const marksT = `${examglobal}.${co}.${qid}.totalMarks`;
     const updateDoc={
         $set: {
             [marksO]:marksOb,
@@ -79,16 +81,92 @@ app.post("/insert",async(req,res)=>{
     }
     const result = await course.updateOne(filter, updateDoc,options);
     
-    return res.redirect('insert.html')
+    return res.redirect('updatestudent.html')
 
 })
+
+
+app.post("/createcsv",async(req,res)=>{
+    courseglobal = req.body.course;
+    examglobal = req.body.exam;
+    const filter = {_id:`${examglobal}schema`};
+    const options = { upsert: true };
+    const course = db.collection(courseglobal.toString());
+    const updateDoc={
+        $set: {
+            _id:`${examglobal}schema`,
+            "name": "name"
+        }
+    }
+    const result = await course.updateOne(filter, updateDoc,options);
+    return res.redirect('/updatecsv.html');
+})
+
+app.post("/updatecsv",async(req,res)=>{
+    let co = req.body.co;
+    let qid = req.body.qid;
+    const filter = {_id:`${examglobal}schema`};
+    const options = { upsert: true };
+    const course = db.collection(courseglobal.toString());
+    const marksObkey = `${qid}Ob`;
+    const marksTkey = `${qid}T`;
+    const marksObvalue = `${examglobal}.co${co}.${qid}.marksObtained`;
+    const marksTvalue = `${examglobal}.co${co}.${qid}.totalMarks`;
+    const updateDoc={
+        $set: {
+            [marksObkey]:marksObvalue,
+            [marksTkey]:marksTvalue
+        }
+    }
+    const result = await course.updateOne(filter, updateDoc,options);
+    
+    return res.redirect('updatecsv.html')
+
+})
+
+
+
+
+
+app.post('/exportcsv',async(req,res)=>{
+    let parserObj = new Parser();
+    const doc = await db.collection(courseglobal.toString()).findOne(
+    {
+        _id:`${examglobal}schema`
+    },
+    (err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        let csv = parserObj.parse(result);
+        fs.writeFile('./data.csv',csv,callback);
+        function callback()
+        {
+            return res.redirect('/exportdatadownload');
+        }
+    }
+    )
+});
+
+
+app.get('/csvdownload', (req, res) => {
+    const filePath = `${__dirname}/data.csv`;
+
+    const stream = fs.createReadStream(filePath);
+
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'inline; filename="mydata.csv"');
+
+    stream.pipe(res);
+});
 
 app.get("/",(req,res)=>{
     res.set({
         "Allow-access-Allow-Origin": '*'
     })
-    return res.redirect('index.html');
+    return res.redirect('home.html');
 }).listen(port);
 
 
 console.log(`Listening on PORT ${port}`);
+
+    // "start": "node index.js"
